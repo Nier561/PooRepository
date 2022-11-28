2e3e092f474a3cb5fce70f7dc462c6ba9c6c5c19
ronny <ronny@10.0.0.219>
2022-11-27 22:10:05 -0400

    Ventana Principla del Programa

--- Proyecto_Final/src/visual/Principal.java
+++ Proyecto_Final/src/visual/Principal.java
@@ -7,6 +7,11 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Principal extends JDialog {
 
@@ -29,25 +34,63 @@ public class Principal extends JDialog {
 	 * Create the dialog.
 	 */
 	public Principal() {
-		setBounds(100, 100, 450, 300);
+		
+		setBounds(100, 100, 900, 700);
+		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			JMenuBar menuBar = new JMenuBar();
+			setJMenuBar(menuBar);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JMenu mnPersonal = new JMenu("Personal");
+				menuBar.add(mnPersonal);
+				{
+					JMenuItem mntmRegPersonal = new JMenuItem("Registrar Personal");
+					mntmRegPersonal.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							RegPersonal regPersonal = new RegPersonal();
+							regPersonal.setVisible(true);
+						}
+					});
+					mnPersonal.add(mntmRegPersonal);
+				}
+				{
+					JMenuItem mntmRegistroSolPer = new JMenuItem("Registrar Solicitud de Personal");
+					mntmRegistroSolPer.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							RegSolicitudPersonal regSolPersonal = new RegSolicitudPersonal();
+							regSolPersonal.setVisible(true);
+						}
+					});
+					mnPersonal.add(mntmRegistroSolPer);
+				}
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JMenu mnCentro = new JMenu("Centro");
+				menuBar.add(mnCentro);
+				{
+					JMenuItem mntmRegistroCentro = new JMenuItem("Registrar Centro");
+					mntmRegistroCentro.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							RegCentro regCentro = new RegCentro();
+							regCentro.setVisible(true);
+						}
+					});
+					mnCentro.add(mntmRegistroCentro);
+				}
+				{
+					JMenuItem mntmRegistroSolCentro = new JMenuItem("Registrar Solicitud de Centro");
+					mntmRegistroSolCentro.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							RegSolicitudCentro regSolCentro = new RegSolicitudCentro();
+							regSolCentro.setVisible(true);
+						}
+					});
+					mnCentro.add(mntmRegistroSolCentro);
+				}
 			}
 		}
 	}
